2200eab1388ec9cbb9fa4dcebc6320e4f9ac9969
KentaOsabe <kenta.osabe@dragon.jp>
2024-09-02 17:22:16 +0900

    no message

---
+++ src/components/OneInfo.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+	title: string;
+	content: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+	<section class="box">
+		<h3>{{title}}</h3>
+		<p>{{content}}</p>
+	</section>
+</template>
+
+<style scoped>
+.box {
+	border: green 1px solid;
+	margin: 10px;
+	color: red;
+}
+</style>
